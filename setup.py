5c1a6cacdeabe5c756b168f209a94b93fb4f60c7
Abhishek Anand <abhishekanand2a@gmail.com>
2021-04-21 11:01:49 +0530

    some changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open('Readme.md') as file:
     longDescription = file.read()
 
-setup(name='UndoRedo',
+setup(name='Undo_Redo',
       version='1.0',
       description='Implementation of Undo, Redo',
       long_description=longDescription,
@@ -11,6 +11,6 @@ setup(name='UndoRedo',
       author='Abhishek Anand',
       author_email="abhishekanand2a@gmail.com",
       license='MIT',
-      packages=['UndoRedo'],
+      packages=['Undo_Redo'],
       install_requires=[],
       )
